59fb9927a71584458d370c4b4c32b1d96434048f
Daniel Fitz <dfitz360@gmail.com>
2017-04-20 14:07:10 +1000

    Changed where pdfs are compiled

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-pp -img=$1 -pdf $1/*.md | pandoc --listings --highlight-style pygments --wrap=preserve -F pandoc-crossref -F pandoc-citeproc --template=./template.latex -f markdown -s -o $1.pdf
+project_dir=$1
+output_name=`echo $project_dir | sed -e 's/\//./g'`
+pp -img=$project_dir -pdf $project_dir/*.md | pandoc --listings --highlight-style pygments --wrap=preserve -F pandoc-crossref -F pandoc-citeproc --template=./template.latex -f markdown -s -o $output_name.pdf
